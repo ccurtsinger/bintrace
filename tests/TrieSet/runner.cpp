24485c7276265f484c66fa91424d3b5e642bd6a6
Charlie Curtsinger <charlie@cs.umass.edu>
2012-08-13 14:43:06 -0400

    Real tests for the TrieSet data structure.

--- tests/TrieSet/runner.cpp
+++ tests/TrieSet/runner.cpp
@@ -1,26 +1,67 @@
+#include <cstdlib>
+#include "TrieSet.hpp"
+
 #include <gtest/gtest.h>
 
-class HelloTest : public testing::Test {
+using namespace std;
+
+enum { Items = 10000 };
+
+class TrieSetTest : public testing::Test {
 protected:
-	virtual void SetUp() {
-		
-	}
+	TrieSet<int> set;
+	int contents[Items];
 	
-	virtual void TearDown() {
-		
-	}	
+	virtual void SetUp() {
+		// Add random elements to the TrieSet and an array
+		for(size_t i=0; i<Items; i++) {
+			int x = rand();
+			set.add(x);
+			contents[i] = x;
+		}
+	}
 };
 
-TEST_F(HelloTest, TestA) {
-	EXPECT_TRUE(true);
+// Make sure everything in the array is also in the set
+TEST_F(TrieSetTest, ContainsTest) {
+	for(size_t i=0; i<Items; i++) {
+		EXPECT_TRUE(set.lookup(contents[i]));
+	}
 }
 
-TEST_F(HelloTest, DISABLED_TestB) {
-	EXPECT_TRUE(false);
+// Make sure things that aren't in the array are also not in the set
+TEST_F(TrieSetTest, NotContainsTest) {
+	size_t count = 0;
+	while(count < Items) {
+		int x = rand();
+		bool found = false;
+		for(size_t i=0; i<Items && !found; i++) {
+			if(contents[i] == x) {
+				found = true;
+			}
+		}
+		
+		if(!found) {
+			EXPECT_FALSE(set.lookup(x));
+			count++;
+		}
+	}
 }
 
-TEST_F(HelloTest, TestC) {
-	EXPECT_TRUE(1 < 2);
+// Make sure the next method always returns the next-largest entry
+TEST_F(TrieSetTest, NextTest) {
+	for(size_t i=0; i<Items; i++) {
+		int x = rand();
+		
+		int min_next = 0;
+		for(size_t j=0; j<Items; j++) {
+			if(contents[j] >= x && (contents[j] < min_next || min_next == 0)) {
+				min_next = contents[j];
+			}
+		}
+		
+		EXPECT_EQ(set.next(x), min_next);
+	}
 }
 
 int main(int argc, char** argv) {
