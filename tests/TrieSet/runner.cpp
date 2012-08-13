99c00b28f4ae61cbfcf11e7887fc8ea02bfe3351
Charlie Curtsinger <charlie@cs.umass.edu>
2012-08-13 13:51:17 -0400

    Forget you, cppunit.  Google Test is better.

--- tests/TrieSet/runner.cpp
+++ tests/TrieSet/runner.cpp
@@ -1,64 +1,27 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
-#include <cppunit/TestFixture.h>
-#include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/CompilerOutputter.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/TestResult.h>
-#include <cppunit/TestResultCollector.h>
-#include <cppunit/TestRunner.h>
-#include <cppunit/BriefTestProgressListener.h>
-#include <cppunit/XmlOutputter.h>
-
-using namespace std;
-using namespace CPPUNIT_NS;
-
-class HelloTest : public TestFixture {
-	CPPUNIT_TEST_SUITE(HelloTest);
-	CPPUNIT_TEST(testA);
-	CPPUNIT_TEST(testB);
-	CPPUNIT_TEST_SUITE_END ();
-
-public:
-	void setUp() {
+class HelloTest : public testing::Test {
+protected:
+	virtual void SetUp() {
 		
 	}
 	
-	void tearDown() {
+	virtual void TearDown() {
 		
-	}
-	
-	void testA() {
-		CPPUNIT_ASSERT(true);
-	}
-	
-	void testB() {
-		CPPUNIT_ASSERT(false);
-	}
+	}	
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(HelloTest);
+TEST_F(HelloTest, TestA) {
+	EXPECT_TRUE(true);
+}
+
+TEST_F(HelloTest, TestB) {
+	EXPECT_TRUE(false);
+}
 
 int main(int argc, char** argv) {
-	TestResult result;
-	
-	TestResultCollector collector;
-	result.addListener(&collector);
-	
-	BriefTestProgressListener progress;
-	result.addListener(&progress);
-	
-	TestRunner runner;
-	runner.addTest(TestFactoryRegistry::getRegistry().makeTest());
-	runner.run(result);
-	
-	ofstream outfile("results.xml");
-	XmlOutputter output(&collector, outfile);
-	output.write();
-	outfile.close();
-	
-	//CompilerOutputter output(&collector, cerr);
-	//output.write();
+	testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
 	
 	return 0;
 }
